f28d42f84234ae10c3dcd9b2f1056598f08e164f
sanya <alexgalayda@gmail.com>
2020-03-01 23:56:06 +0300

    added bin to hex

--- Vis/Gen_Nms_1s.v
+++ Vis/Gen_Nms_1s.v
@@ -3,11 +3,11 @@ module Gen_Nms_1s(
 	input ce
 );
 parameter F1kHz = 1000;
-reg[9:0]cb_Nms = 0;
+reg[9:0]cb_ms = 0;
 wire[9:0]Nms = F1kHz;
  
 assign CEO = ce & (cb_ms==1);
 always @(posedge clk) if (ce) begin
-	cb_Nms <= (cb_ms==1)? Nms : cb_Nms-1 ;
+	cb_ms <= (cb_ms==1)? Nms : cb_ms-1 ;
 end
 endmodule
--- Vis/Vis.v
+++ Vis/Vis.v
@@ -1,13 +1,10 @@
-//19 - VCD4RE <- VCB4CLED <- VCGrey4RE <-VCJ4RE
-//VCB4CLED <- VCB4RE <- VCD4RE <- VCJ4RE
 module Vis(
 	input F50MHz, 	output wire [3:0] AN , 
 	input [7:0]SW,	output wire [7:0] SEG,
-	input BTN0, 	output wire [7:0] LED, 
-	input BTN3
+						output wire [7:0] LED
 	);
 				
-wire CEO01;//, CEO12, CEO23, CEO34, CEO45;
+//wire CEO01;//, CEO12, CEO23, CEO34, CEO45;
 //wire TC1, TC2, TC3, TC4;
 wire CE1MS;
 wire [15:0] dat;
@@ -15,50 +12,13 @@ wire [15:0] dat;
 wire clk;
 BUFG DD0 (.I(F50MHz), .O(clk));
 
-Gen_Nms_1s DD1(
-	.clk(clk),	.CEO(CEO01),
-	.ce(CE1MS)
-);
-
-//Gen_Nms_1s DD1(
-//	.clk(clk),	.CEO(CEO01),
-//	.ce(CE1MS)
-//);
-
-/*VCJ4RE DD2(
-	.ce(CEO01), .TC(TC1), 
-  .clk(clk), .CEO(CEO12),
-  .R(BTN0), .Q(dat[3:0])
-);
-
-VCGrey4Re DD3(
-	.clk(clk),.Y(dat[7:4]),
-	.ce(CEO12), .CEO(CEO23),
-	.r(BTN0), .TC(TC2)
-);
-
-VCBmCLED DD4(
-	.ce(CEO23), .Q(dat[11:8]),
-	.up(SW[6]), .CEO(CEO34),
-	.di(SW[3:0]), .TC(TC3),
-	.L(BTN3),
-	.clk(clk),
-	.clr(BTN0)
-);
-			
-VCD4RE DD5(
-	.clk(clk), .TC(TC4),
-	.ce(CEO34), .CEO(CEO45),
-	.R(BTN0), .Q(dat[15:12])
-);*/
-
 //clk -- часы
 //dat -- 16-ти битное число
 //PTR -- точка разделитель
 //AN -- выбор, какую из 4 цифр дисплея использовать
 //SEG -- кодировка цифры для дисплея
 //se1ms -- CEO для дисплея
-assign dat = 15;
+assign dat = SW;
 DISPLAY DD6(
 	.clk(clk), .AN(AN),
 	.dat(dat), .SEG(SEG),
