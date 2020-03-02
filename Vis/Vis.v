ed84237df7b23a81e5eabc3799793bbac253bdf8
sanya <alexgalayda@gmail.com>
2020-03-02 17:17:48 +0300

    added bin to hex with button

--- Vis/Vis.v
+++ Vis/Vis.v
@@ -1,7 +1,7 @@
 module Vis(
 	input F50MHz, 	output wire [3:0] AN , 
 	input [7:0]SW,	output wire [7:0] SEG,
-						output wire [7:0] LED
+	input BTN0,		output wire [7:0] LED
 	);
 
 wire CE1MS;
@@ -9,18 +9,22 @@ wire [15:0] dat;
 
 wire clk;
 BUFG DD0 (.I(F50MHz), .O(clk));
-	
+
+reg Q = 0;
+always @(posedge clk)
+	Q <= BTN0? !Q : Q;
+
 //clk -- часы
 //dat -- 16-ти битное число
 //PTR -- точка разделитель
 //AN -- выбор, какую из 4 цифр дисплея использовать
 //SEG -- кодировка цифры для дисплея
 //se1ms -- CEO для дисплея
-assign dat = SW[6:0];
+assign dat = SW[7:0];
 DISPLAY DD6(
 	.clk(clk), .AN(AN),
 	.dat(dat), .SEG(SEG),
-	.PTR(SW[7]), .ce1ms(CE1MS)
+	.PTR(Q), .ce1ms(CE1MS)
 );
 
 OBUF8 DD7(.I(SW[7:0]), .O(LED[7:0]));
